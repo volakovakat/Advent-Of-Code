c85de0e755c6a1d7687c44cd5cfa2daecd95e91f
Katerina Volakova <volakova.kat@gmail.com>
2022-12-04 17:22:45 +0100

    day 4

---
+++ 2022/day_04/task2.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { Duplex } = require('stream');
+
+let inputData = [];
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  inputData = data.split('\n');
+
+  let duplicationList = [];
+
+  for (let i = 0; i < inputData.length; i++) {
+    let splitPairs = inputData[i].split(",");
+    let pair1 = splitPairs[0].split("-");
+    let pair2 = splitPairs[1].split("-");
+
+    let interval1 = [];
+    let interval2 = [];
+
+    for (let j = Number(pair1[0]); j <= Number(pair1[1]); j++) {
+      interval1.push(j);
+    }
+
+    for (let j = Number(pair2[0]); j <= Number(pair2[1]); j++) {
+      interval2.push(j);
+    }
+    const duplicates = interval1.filter(element => interval2.includes(element));
+    const duplicatesReverse = interval2.filter(element => interval1.includes(element));
+    result = duplicates.length;
+
+    if (duplicates.length > 0 || duplicatesReverse.length > 0) {
+      duplicationList.push(1);
+    }
+  }
+  console.log(duplicationList.reduce((a, b) => a + b, 0));
+});
